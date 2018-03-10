5ab46e43556718cbf814368db9164f4b2cfd0a05
sensei55 <senseit55@ya.ru>
2018-03-10 12:27:12 +0300

    Revert "Арифметические операции"
    This reverts commit 4a089172455fa2d921bfbd49bfc6e9902b5d0b3d.

--- sandbox/src/main/java/ru/wrtest/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/wrtest/sandbox/MyFirstProgram.java
@@ -1,23 +1,10 @@
 package ru.wrtest.sandbox;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        System.out.println(2 + 2);
-        System.out.println(2 - 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2.0);
-
-        System.out.println("2 + 2 = " + (2 + 2));
     }
 
-
-
+    
 }
