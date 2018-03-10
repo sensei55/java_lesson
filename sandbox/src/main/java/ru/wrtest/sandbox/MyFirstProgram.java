d596693c0324a7c9a75f63942cc99cd758b0cc6f
sensei55 <senseit55@ya.ru>
2018-03-10 04:00:19 +0300

    test

--- sandbox/src/main/java/ru/wrtest/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/wrtest/sandbox/MyFirstProgram.java
@@ -1,8 +1,10 @@
 package ru.wrtest.sandbox;
 
 public class MyFirstProgram {
- public static void main(String[] args) {
-    System.out.println("Hello world!");
- }
- 
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    
 }
